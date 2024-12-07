5439c4a591c6b0a1195751c5996bac91cdbec871
ah1028 <amyhersey04@gmail.com>
2024-12-07 17:30:47 +0000

    Finished day 5 part 1

---
+++ Day5.py
@@ -0,0 +1,42 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+with open("rules.txt", "r") as f:
+    rules = [l.strip() for l in f.readlines()]
+with open("pages.txt","r") as f:
+    pages = [l.strip() for l in f.readlines()]
+
+edges = {}
+for line in rules:
+    first, second = map(str.strip, line.split("|"))
+    if second not in edges:
+        edges[second] = [first]
+    else:
+        temp = edges[second] + [first]
+        edges[second] = temp
+
+
+valid = []
+for page in pages:
+    v = True
+    pg = page.split(",")
+    for a in range(0, len(pg)):
+        entries = edges.get(pg[a])
+        if entries != None:
+            pg[a] = ""
+            res = (set(entries)).intersection(set(pg))
+            if res:
+                v = False
+                break
+        pg[a] = ""
+    if v:
+        valid.append(page)
+
+total = 0
+for b in valid:
+    b = b.split(",")
+    mid = (len(b) - 1) // 2
+    total += int(b[mid])
+
+print(total)
+
